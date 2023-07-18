16234e280b4b4fe90073b308039afbb4abfff9c1
Chenxi <lin.chenxi14@gmail.com>
2023-07-18 19:11:21 -0400

    added more style to the calculator

--- src/components/buttons/Buttons.tsx
+++ src/components/buttons/Buttons.tsx
@@ -15,48 +15,49 @@ export default function Buttons(props: any) {
     
     return (
         <div id="button-wrapper">
-            {console.log(decimal)}
-            {buttons.map((obj: any) => (
-                <button key={obj.id} className='button' id={obj.id} onClick={() => {
-                    
-                    
-                    if(view === '0') {
-                        setView(obj.symb)
-                        setDecimal(obj.symb)
-                    } else {
-                        setView(view + obj.symb)
-                        setDecimal(decimal + obj.symb)
-                    }
+            <div className='button-container'>
+                {buttons.map((obj: any) => (
+                    <button key={obj.id} className='button' id={obj.id} onClick={() => {
+                        
+                        
+                        if(view === '0') {
+                            setView(obj.symb)
+                            setDecimal(obj.symb)
+                        } else {
+                            setView(view + obj.symb)
+                            setDecimal(decimal + obj.symb)
+                        }
 
-                    if(obj.symb === '/' || obj.symb === '+' || obj.symb === '*' || obj.symb === '-') {
-                        setView(decimal + obj.symb)
-                        setDecimal('')
-                            if(view !== decimal) {
-                                setView(view + obj.symb)
-                            }
-                    }
+                        if(obj.symb === '/' || obj.symb === '+' || obj.symb === '*' || obj.symb === '-') {
+                            setView(decimal + obj.symb)
+                            setDecimal('')
+                                if(view !== decimal) {
+                                    setView(view + obj.symb)
+                                }
+                        }
 
-                    if(obj.symb === '=') {
-                        // eslint-disable-next-line
-                        let filter = view.match(/(\*|\+|\/|-)?(\.|\-)?\d+/g).join('');
-                        setView(Parser.evaluate(filter).toString());
-                    }
+                        if(obj.symb === '=') {
+                            // eslint-disable-next-line
+                            let filter = view.match(/(\*|\+|\/|-)?(\.|\-)?\d+/g).join('');
+                            setView(Parser.evaluate(filter).toString());
+                        }
 
-                    if(obj.symb === '.') {
-                        setDecimal(decimal + obj.symb)
-                        setView(view + obj.symb)
-                            if(decimal.includes('.')) {
-                                setDecimal(decimal)
-                                setView(view)
-                            }
-                    }
+                        if(obj.symb === '.') {
+                            setDecimal(decimal + obj.symb)
+                            setView(view + obj.symb)
+                                if(decimal.includes('.')) {
+                                    setDecimal(decimal)
+                                    setView(view)
+                                }
+                        }
 
-                    if(obj.symb === 'AC') {
-                        symbol(obj.symb)
-                    }
-                    
-                }} >{obj.symb}</button>
-            ))}
+                        if(obj.symb === 'AC') {
+                            symbol(obj.symb)
+                        }
+                        
+                    }} >{obj.symb}</button>
+                ))}
+            </div>
         </div>
     )
 }
